16176f8fa087f1ebeb3c6667470772d08e4c23d8
Luis Miguel Barco <luismiguelbarco@LFRW0164.liferay.org.es>
2018-09-17 10:41:51 +0200

    Change app.js to a better code

--- AwesomeApp/App.js
+++ AwesomeApp/App.js
@@ -7,77 +7,78 @@
  */
 
 import React, {Component} from 'react';
-import { Platform ,StyleSheet ,Dimensions, View} from 'react-native';
+import {Platform, Dimensions,  StyleSheet, Text, View} from 'react-native';
 
-//Screenlets
-import { LoginScreenlet } from './LiferayScreens'
-import { UserPortraitScreenlet } from './LiferayScreens'
-import { ImageGalleryScreenlet } from './LiferayScreens'
+// Screenlets
+import { LoginScreenlet } from './LiferayScreens';
+import { UserPortraitScreenlet } from './LiferayScreens';
+import { ImageGalleryScreenlet } from './LiferayScreens';
 
+export default class App extends Component {
+  constructor(props){
+    super(props);
 
-export default class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          logged: false
-        };
-        this._onLoginSuccess = this._onLoginSuccess.bind(this);    
+    this.state = {
+      logged: false
     }
-    
-    render() {
-        if(this.state.logged) {
-            return (
-                <View style={styles.container}>
-                  <UserPortraitScreenlet 
-                    style={styles.portrait}
-                    userId={this.state.userId}
-                  />
-                  <ImageGalleryScreenlet 
-                    style={styles.gallery}
-                    folderId={72155}
-                    repositoryId={20143}
-                  />
-                </View>
-            );
-        } else {
-            return (
-            <View style={styles.container}>
-                <LoginScreenlet 
-                style={styles.login}
-                onLoginSuccess={this._onLoginSuccess} />
-            </View>
-            );
-        }
+  }
+  
+  render() {
+    if(this.state.logged) {
+      return(
+        <View style={styles.container}>
+            <UserPortraitScreenlet 
+              style={styles.portrait}
+              userId={this.state.userId}
+            />
+            <ImageGalleryScreenlet 
+              style={styles.gallery}
+              folderId={72155}
+              repositoryId={20143}
+            />
+        </View>
+      );
     }
+    return (
+      <View style={styles.container}>
+        <LoginScreenlet 
+          style={styles.login}
+          onLoginSuccess={this._onLoginSuccess.bind(this)}
+        />
+      </View>
+    );
+  }
 
-    //Login success
-    _onLoginSuccess(userId) {
-        this.setState({
-            logged: true,
-            userId: parseInt(userId.userId)
-        });
-    }
+  _onLoginSuccess(userAttributes) {
+    this.setState(previousState => {
+      return {
+        logged: !previousState.logged,
+        userId: parseInt(userAttributes.userId)
+      }
+    });
+  }
 }
-// const {height, width} = Dimensions.get('window')
-const {height, width} = Dimensions.get('window');
 
+const {width} = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
-    flex: 1,    
-    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'flex-start'
-  },
-  portrait: {
-    height: 150,
-    width: 150
-  },
-  gallery: {
-    height: height,
-    width: Platform.OS === 'ios' ? width : 450
+    backgroundColor: '#F5FCFF',
   },
   login:{
     height:300,
     width:300
+  },
+  portrait:{
+    height: 150,
+    width: 150
+  },
+  gallery: {
+    height: 500,
+    width: width
   }
+  
+  
 });
